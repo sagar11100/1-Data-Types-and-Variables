a731664f9536b6799cf4da9dfd8cec22a8288cb0
sagar11100 <91186410+sagar11100@users.noreply.github.com>
2024-11-05 13:57:25 +0530

    Add files via upload

---
+++ Names.js
@@ -0,0 +1,6 @@
+var name = "Sagar";
+console.log(name);
+var fName = "Jairam";
+console.log(fName);
+var mName = "Sandhya";
+console.log(mName);
---
+++ Typeof.js
@@ -0,0 +1,4 @@
+var name = "Sagar";
+var age = 25;
+console.log(name, age);
+console.log(typeof(name), typeof(age));

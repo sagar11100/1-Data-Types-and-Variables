53577c633371213dd4d235c22c7fd4f82e392755
sagar11100 <91186410+sagar11100@users.noreply.github.com>
2024-11-05 13:49:06 +0530

    Add files via upload

---
+++ Marksheet.js
@@ -0,0 +1,16 @@
+console.log("************************************");
+console.log("              Report Card           ");
+console.log("************************************");
+console.log("Student Name : Sagar Vishwakarma");
+console.log("Roll No. : 54");
+console.log("Class : 10th");
+console.log("School Name : St. George High School");
+console.log("************************************");
+console.log("Subjects             Grades");
+console.log("************************************");
+console.log("Mathematics            A");
+console.log("Science                B+");
+console.log("History                B");
+console.log("************************************");
+console.log("          End of Report Card        ");
+console.log("************************************");
